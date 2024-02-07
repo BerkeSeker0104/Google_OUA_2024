b2ac2f98449a72cf504097065dab716fc06dde42
Berke Seker <berkeseker.2000@gmail.com>
2024-02-07 16:33:22 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  const dart_const = "I Love Dart";
+  print(dart_const);
+
+  final dart_final = "This is Final Value";
+  print(dart_final);
+
+  var dart_var = "This is a Value";
+  print(dart_var);
+
+  dart_var = "this is an updated value";
+  print(dart_var);
+}

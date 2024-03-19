284d72524e041c1f6ef32914077433525fdc4ea2
Berke Seker <berkeseker.2000@gmail.com>
2024-03-19 16:46:12 +0300

    ders icerigi guncellendi

--- flutter_course/flutter101/lib/main.dart
+++ flutter_course/flutter101/lib/main.dart
@@ -1,5 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text()));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 153, 106, 235),
+                Color.fromARGB(255, 64, 22, 138),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
